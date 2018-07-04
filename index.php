7edba5a80d305dddc404206437bf40f44405fa63
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-07-03 17:20:13 -0700

    Fixed variable bug

--- index.php
+++ index.php
@@ -25,8 +25,8 @@
       $sensor_log_files = scandir($sensor_log_dir);
       $sensor_log_files = array_diff($sensor_log_files, array('.', '..')); # Remove . and ..
 
-      $sensor_data_files = scandir($sensor_data_files);
-      // $sensor_data_files = array_diff($sensor_data_files, array('.', '..')); # Remove . and ..
+      $sensor_data_files = scandir($sensor_data_dir);
+      $sensor_data_files = array_diff($sensor_data_files, array('.', '..')); # Remove . and ..
 
       foreach($sensor_data_files as $file) {
         echo "<a href='download.php?file=".$file."'>".$file."</a><br>";
