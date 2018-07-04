088adbb699cefa484be50b854edf3f44677a8d36
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-07-03 17:19:31 -0700

    Added HTML structure

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
-<?php
-  /* This file lists all sensor data and logs gathered by the data mule
-     and makes the data available for download on the client. */
+<!doctype html>
 
-  $sensor_log_dir = "/srv/logs/";
-  $sensor_data_dir = "/srv/";
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
 
-  $sensor_log_files = scandir($sensor_log_dir);
-  $sensor_log_files = array_diff($sensor_log_files, array('.', '..')); # Remove . and ..
+    <title>Mission Mule</title>
+    <meta name="description" content="Download retrieved data from data stations">
 
-  $sensor_data_files = scandir($sensor_data_files);
-  $sensor_data_files = array_diff($sensor_data_files, array('.', '..')); # Remove . and ..
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
 
-  foreach($sensor_data_files as $file) {
-    echo "<a href='download.php?file=".$file."'>".$file."</a><br>";
-  }
- ?>
+    <!--[if lt IE 9]>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <?php
+      /* This file lists all sensor data and logs gathered by the data mule
+         and makes the data available for download on the client. */
+
+      $sensor_log_dir = "/srv/logs/";
+      $sensor_data_dir = "/srv/";
+
+      $sensor_log_files = scandir($sensor_log_dir);
+      $sensor_log_files = array_diff($sensor_log_files, array('.', '..')); # Remove . and ..
+
+      $sensor_data_files = scandir($sensor_data_files);
+      // $sensor_data_files = array_diff($sensor_data_files, array('.', '..')); # Remove . and ..
+
+      foreach($sensor_data_files as $file) {
+        echo "<a href='download.php?file=".$file."'>".$file."</a><br>";
+      }
+     ?>
+  </body>
+</html>
