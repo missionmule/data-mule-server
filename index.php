2f4457525f35a18222013d6d9154b83b541cac8f
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-08-02 16:02:39 -0700

    Moved zip to download

--- index.php
+++ index.php
@@ -1,7 +1,56 @@
 <?php
 // Create ZIP file
 if(isset($_POST['create'])){
+ $zip = new ZipArchive();
+ $filename = "/tmp/flight-data.zip";
 
+ if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
+  exit("cannot open <$filename>\n");
+ }
+
+ $dir = '/srv/flight-data';
+
+ // Create zip
+ createZip($zip,$dir);
+
+ $zip->close();
+}
+
+// Create zip
+function createZip($zip,$dir){
+ if (is_dir($dir)){
+
+  if ($dh = opendir($dir)){
+   while (($file = readdir($dh)) !== false){
+
+    // If file
+    if (is_file($dir.$file)) {
+     if($file != '' && $file != '.' && $file != '..'){
+
+      $zip->addFile($dir.$file);
+     }
+    }else{
+     // If directory
+     if(is_dir($dir.$file) ){
+
+      if($file != '' && $file != '.' && $file != '..'){
+
+       // Add empty directory
+       $zip->addEmptyDir($dir.$file);
+
+       $folder = $dir.$file.'/';
+
+       // Read data of the folder
+       createZip($zip,$folder);
+      }
+     }
+
+    }
+
+   }
+   closedir($dh);
+  }
+ }
 }
 
 // Download Created Zip file
@@ -58,6 +107,8 @@ if(isset($_POST['download'])){
    }
   }
 
+ $filename = "/tmp/flight-data.zip";
+
  if (file_exists($filename)) {
   header('Content-Type: application/zip');
   header('Content-Disposition: attachment; filename="'.basename($filename).'"');
@@ -81,6 +132,7 @@ if(isset($_POST['download'])){
         <div class='container'>
             <h1>Create and Download UAS Data</h1>
         <form method='post' action=''>
+            <input type='submit' name='create' value='Create Zip' />&nbsp;
             <input type='submit' name='download' value='Download' />
         </form>
         </div>
