f01d5e64b50a99c9abd8c9b9d5dd46a7a6c958d3
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-08-02 14:45:04 -0700

    Removed headers

--- zip.php
+++ zip.php
@@ -50,15 +50,15 @@
 
   $zip = createZipFromDir('/srv/', 'srv');
   $zip->close()
-
-  header('Content-Description: File Transfer');
-  header('Content-Type: application/octet-stream');
-  header('Content-Disposition: attachment; filename='.basename("srv"));
-  header('Content-Transfer-Encoding: binary');
-  header('Expires: 0');
-  header('Cache-Control: must-revalidate');
-  header('Pragma: public');
-  header('Content-Length: ' . filesize($zip));
+  // 
+  // header('Content-Description: File Transfer');
+  // header('Content-Type: application/octet-stream');
+  // header('Content-Disposition: attachment; filename='.basename("srv"));
+  // header('Content-Transfer-Encoding: binary');
+  // header('Expires: 0');
+  // header('Cache-Control: must-revalidate');
+  // header('Pragma: public');
+  // header('Content-Length: ' . filesize($zip));
   readfile($zip);
 
 ?>
