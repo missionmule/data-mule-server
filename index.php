c1ae332faa76f435996a346df0ffb220a4484379
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-08-02 13:47:30 -0700

    Simplified to zip download

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
          and makes the data available for download on the client. */
 
       $sensor_log_dir = "/srv/logs/";
-      $sensor_data_dir = "/srv/data/";
+      $sensor_data_dir = "/srv/field/";
 
       $sensor_log_files = scandir($sensor_log_dir);
       $sensor_log_files = array_diff($sensor_log_files, array('.', '..')); # Remove . and ..
@@ -28,8 +28,11 @@
       $sensor_data_files = scandir($sensor_data_dir);
       $sensor_data_files = array_diff($sensor_data_files, array('.', '..')); # Remove . and ..
 
-      echo "<h1>Sensor Data</h1>";
+      /*echo "<h1>Sensor Data</h1>";
       foreach($sensor_data_files as $file) {
+        foreach($sensor_data_files as $file) {
+          echo "<a href='download.php?file=".$file."&amp;path=data'>".$file."</a><br>";
+        }
         echo "<a href='download.php?file=".$file."&amp;path=data'>".$file."</a><br>";
       }
 
@@ -37,8 +40,11 @@
       foreach($sensor_log_files as $file) {
         echo "<a href='download.php?file=".$file."&amp;path=logs'>".$file."</a><br>";
       }
+      */
 
-      echo "<a href='zip.php?dir=".$sensor_data_dir."'>Download</a><br>";
+      echo "<a href='zip.php?dir=/srv/'>Download Data</a><br>";
+
+
      ?>
   </body>
 </html>
