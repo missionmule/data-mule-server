ffc0ca9c688334be8cee9d39cf696920cd498456
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-08-02 16:12:55 -0700

    Reverted to old structure

--- index.php
+++ index.php
@@ -1,132 +1,87 @@
 <?php
-// // Create ZIP file
-// if(isset($_POST['create'])){
-//  $zip = new ZipArchive();
-//  $filename = "/tmp/flight-data.zip";
-//
-//  if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
-//   exit("cannot open <$filename>\n");
-//  }
-//
-//  $dir = '/srv/flight-data';
-//
-//  // Create zip
-//  createZip($zip,$dir);
-//
-//  $zip->close();
-// }
-//
-// // Create zip
-// function createZip($zip,$dir){
-//  if (is_dir($dir)){
-//
-//   if ($dh = opendir($dir)){
-//    while (($file = readdir($dh)) !== false){
-//
-//     // If file
-//     if (is_file($dir.$file)) {
-//      if($file != '' && $file != '.' && $file != '..'){
-//
-//       $zip->addFile($dir.$file);
-//      }
-//     }else{
-//      // If directory
-//      if(is_dir($dir.$file) ){
-//
-//       if($file != '' && $file != '.' && $file != '..'){
-//
-//        // Add empty directory
-//        $zip->addEmptyDir($dir.$file);
-//
-//        $folder = $dir.$file.'/';
-//
-//        // Read data of the folder
-//        createZip($zip,$folder);
-//       }
-//      }
-//
-//     }
-//
-//    }
-//    closedir($dh);
-//   }
-//  }
-// }
+// Create ZIP file
+if(isset($_POST['create'])){
+ $zip = new ZipArchive();
+ $filename = "/tmp/flight-data.zip";
 
-  // Create zip
-  function createZip($zip,$dir){
-   if (is_dir($dir)){
+ if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
+  exit("cannot open <$filename>\n");
+ }
 
-    if ($dh = opendir($dir)){
-     while (($file = readdir($dh)) !== false){
+ $dir = '/srv/flight-data';
 
-      // If file
-      if (is_file($dir.$file)) {
-       if($file != '' && $file != '.' && $file != '..'){
+ // Create zip
+ createZip($zip,$dir);
 
-        $zip->addFile($dir.$file);
-       }
-      }else{
-       // If directory
-       if(is_dir($dir.$file) ){
+ $zip->close();
+}
 
-        if($file != '' && $file != '.' && $file != '..'){
+// Create zip
+function createZip($zip,$dir){
+ if (is_dir($dir)){
 
-         // Add empty directory
-         $zip->addEmptyDir($dir.$file);
+  if ($dh = opendir($dir)){
+   while (($file = readdir($dh)) !== false){
 
-         $folder = $dir.$file.'/';
+    // If file
+    if (is_file($dir.$file)) {
+     if($file != '' && $file != '.' && $file != '..'){
 
-         // Read data of the folder
-         createZip($zip,$folder);
-        }
-       }
-
-      }
-
+      $zip->addFile($dir.$file);
      }
-     closedir($dh);
+    }else{
+     // If directory
+     if(is_dir($dir.$file) ){
+
+      if($file != '' && $file != '.' && $file != '..'){
+
+       // Add empty directory
+       $zip->addEmptyDir($dir.$file);
+
+       $folder = $dir.$file.'/';
+
+       // Read data of the folder
+       createZip($zip,$folder);
+      }
+     }
+
     }
+
    }
-  // Download Created Zip file
-  if(isset($_POST['download'])){
+   closedir($dh);
+  }
+ }
+}
 
-    $zip = new ZipArchive();
-    $filename = "/tmp/flight-data.zip";
+// Download Created Zip file
+if(isset($_POST['download'])){
 
-    if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
-     exit("cannot open <$filename>\n");
-    }
+ $filename = "/tmp/flight-data.zip";
 
-    $dir = '/srv/flight-data';
+ if (file_exists($filename)) {
+  header('Content-Type: application/zip');
+  header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+  header('Content-Length: ' . filesize($filename));
 
-    // Create zip
-    createZip($zip,$dir);
+  flush();
+  readfile($filename);
+  // delete file
+  unlink($filename);
 
-    $zip->close();
-
-    if (file_exists($filename)) {
-      header('Content-Type: application/zip');
-      header('Content-Disposition: attachment; filename="'.basename($filename).'"');
-      header('Content-Length: ' . filesize($filename));
-
-      flush();
-      readfile($filename);
-      // delete file
-      unlink($filename);
-    }
+ }
 }
 ?>
 <!doctype html>
 <html>
     <head>
-      <title>Flight Data Download</title>
+      <title>Download Flight Data</title>
       <link href='style.css' rel='stylesheet' type='text/css'>
     </head>
     <body>
         <div class='container'>
-            <h1>Download UAS Data</h1>
+            <h1>Create and Download UAS Data</h1>
         <form method='post' action=''>
+            <input type='submit' name='create' value='Create Zip' />&nbsp;
             <input type='submit' name='download' value='Download' />
         </form>
         </div>
